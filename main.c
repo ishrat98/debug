fc72a8cb28c474e6f8e883f62d5dd601104a6af4
ishrat98 <bsse0927@iit.du.ac.bd>
2018-01-28 19:35:43 -0800

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=5;
+    for(int b=10;b>=0;b--)
+    {
+        int c =50/(b-a);
+        printf("%d\n",c);
+    }
+
+   // printf("Hello world!\n");
+    return 0;
+}
